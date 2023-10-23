e2afefa223714bcbb949869e74ef2ecaa3daa594
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-10-23 09:55:25 +0700

    Initial commit

---
+++ src/main/java/com/bayu/microservicespart1/MicroservicesPart1Application.java
@@ -0,0 +1,13 @@
+package com.bayu.microservicespart1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesPart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesPart1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bayu/microservicespart1/MicroservicesPart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayu.microservicespart1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesPart1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
