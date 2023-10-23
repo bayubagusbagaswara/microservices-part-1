af73cb4c1e9c23a60abc12462076b669755ae479
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-10-23 15:07:16 +0700

    add dto ProductRequest

---
+++ product-service/src/main/java/com/bayu/productservice/dto/ProductRequest.java
@@ -0,0 +1,30 @@
+package com.bayu.productservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+/**
+ * Class Name: com.bayu.productservice.dto
+ * Description: microservices-part-1
+ * <p>
+ * Author: bayub
+ * Date: 23/10/2023
+ * Last Modified: 15:02-23/10/2023-10-2023
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductRequest {
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+}
