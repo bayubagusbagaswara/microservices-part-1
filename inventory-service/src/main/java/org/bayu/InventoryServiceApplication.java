fe94cf9d837fd87e2823767568c2bba51448c6c9
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-10-24 10:32:28 +0700

    add main application Inventory Service

---
+++ inventory-service/src/main/java/org/bayu/InventoryServiceApplication.java
@@ -0,0 +1,14 @@
+package org.bayu;
+
+
+import java.util.logging.Logger;
+
+public class InventoryServiceApplication {
+
+    private static final Logger log = Logger.getLogger(InventoryServiceApplication.class.getName());
+
+    public static void main(String[] args) {
+        log.info("Inventory Service is running...");
+    }
+
+}
