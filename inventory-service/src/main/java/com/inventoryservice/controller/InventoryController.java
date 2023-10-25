a831f4552036f07f1124660bd923b472c932e4d4
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-10-25 14:29:36 +0700

    add controller InventoryController

---
+++ inventory-service/src/main/java/com/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,24 @@
+package com.inventoryservice.controller;
+
+import com.inventoryservice.dto.InventoryResponse;
+import com.inventoryservice.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping("/{sku-code}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
+        return inventoryService.isInStock(skuCode);
+    }
+
+}
