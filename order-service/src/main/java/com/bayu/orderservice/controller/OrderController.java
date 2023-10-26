3d8bb0bd68370c6b943989c029d271d19116b489
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-10-26 09:52:53 +0700

    add OrderController

---
+++ order-service/src/main/java/com/bayu/orderservice/controller/OrderController.java
@@ -0,0 +1,23 @@
+package com.bayu.orderservice.controller;
+
+import com.bayu.orderservice.dto.OrderRequest;
+import com.bayu.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/api/order")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
+        return "Order Placed Successfully";
+    }
+
+}
