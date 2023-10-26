6dd4b35a3df2c20eba1b8f6026db3a8f33109a9d
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-10-26 09:24:32 +0700

    add entity OrderLineItem

---
+++ order-service/src/main/java/com/bayu/orderservice/model/OrderLineItem.java
@@ -0,0 +1,37 @@
+package com.bayu.orderservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_line_items")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderLineItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "sku_code")
+    private String skuCode;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+}
