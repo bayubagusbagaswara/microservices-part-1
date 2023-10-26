385ec7d0ec1b55ed43f87df879f3dfaae24461ef
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-10-26 09:27:36 +0700

    add OrderRepository

---
+++ order-service/src/main/java/com/bayu/orderservice/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.bayu.orderservice.repository;
+
+
+import com.bayu.orderservice.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, String> {
+}
