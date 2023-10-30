bd6a11e46c797c889a6a9378833dfd14e8631293
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-10-30 10:03:24 +0700

    remove annotation not supported for Record

--- order-service/src/main/java/com/bayu/orderservice/dto/OrderRequest.java
+++ order-service/src/main/java/com/bayu/orderservice/dto/OrderRequest.java
@@ -1,16 +1,10 @@
 package com.bayu.orderservice.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 import java.util.List;
 
-@EqualsAndHashCode(callSuper = true)
-@Data
 @Builder
-@AllArgsConstructor
 public record OrderRequest(
         List<OrderLineItemDTO> orderLineItemDTOList
 ) {
