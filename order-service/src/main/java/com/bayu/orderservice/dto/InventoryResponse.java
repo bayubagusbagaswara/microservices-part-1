a5a2d75dba8caea92bb6d7e1639e2c1acf60c8f4
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-10-30 10:03:13 +0700

    remove annotation not supported for Record

--- order-service/src/main/java/com/bayu/orderservice/dto/InventoryResponse.java
+++ order-service/src/main/java/com/bayu/orderservice/dto/InventoryResponse.java
@@ -3,10 +3,7 @@ package com.bayu.orderservice.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
-@EqualsAndHashCode(callSuper = true)
-@Data
 @Builder
-@AllArgsConstructor
 public record InventoryResponse(
         @JsonProperty(value = "sku_code") String skuCode,
         @JsonProperty(value = "is_ins_stock") boolean isInStock
