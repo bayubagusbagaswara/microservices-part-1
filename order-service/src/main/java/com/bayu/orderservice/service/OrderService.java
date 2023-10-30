65e39719c935532d3453c6a9795906f004ab054a
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-10-30 11:04:22 +0700

    add comments and notes to see the response from the inventory service

--- order-service/src/main/java/com/bayu/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/bayu/orderservice/service/OrderService.java
@@ -7,6 +7,7 @@ import com.bayu.orderservice.model.Order;
 import com.bayu.orderservice.model.OrderLineItem;
 import com.bayu.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +19,7 @@ import java.util.Objects;
 import java.util.UUID;
 
 @Service
+@Slf4j
 @Transactional
 @RequiredArgsConstructor
 public class OrderService {
@@ -29,7 +31,17 @@ public class OrderService {
     @Value("${url.api.inventory}")
     private static String apiInventoryURL; // http://localhost:8082/api/inventory
 
+    // ketika customer mau membuat pesanan (order)
+    // maka perlu cek stock di inventory-service, apakah stock masih ada
+
     public void placeOrder(OrderRequest orderRequest) {
+        /**
+         * Order Request mengirim data
+         * skuCode,
+         * price,
+         * quantity
+         * data-data skuCode dan price harusnya didapatkan dari product-service. Sedangkan quantity adalah jumlah yang ingin dibeli
+         */
         Order order = Order.builder()
                 .orderNumber(UUID.randomUUID().toString())
                 .build();
@@ -53,12 +65,18 @@ public class OrderService {
                 .bodyToMono(InventoryResponse[].class)
                 .block();
 
+        for (InventoryResponse inventoryResponse : inventoryResponsesArray) {
+            log.info("Inventory SKU Code : {}", inventoryResponse.skuCode());
+            log.info("Inventory Stock : {}", inventoryResponse.isInStock());
+        }
+
         // stream data isInStock
         boolean allProductsInStock = Arrays.stream(Objects.requireNonNull(inventoryResponsesArray))
                 .allMatch(InventoryResponse::isInStock);
 
         // if result = true, then save the order, if else then throw error
         if (Boolean.TRUE.equals(allProductsInStock)) {
+            // jika stock product masih tersedia
             orderRepository.save(order);
         } else {
             throw new IllegalArgumentException("Product is not in stock, please try again later");
