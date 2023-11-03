8408851db56a23157cc17e31422abe678bf2ba14
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-11-03 13:36:36 +0700

    add annotation ApiGatewayApplication

--- api-gateway/src/main/java/com/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/apigateway/ApiGatewayApplication.java
@@ -2,10 +2,12 @@ package com.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import java.util.logging.Logger;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
     private static final Logger log = Logger.getLogger(ApiGatewayApplication.class.getName());
